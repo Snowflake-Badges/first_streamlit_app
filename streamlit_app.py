10d950b20ab48806e8b5e1f659f8a044bb11add4
Snowflake-Badges <149055697+Snowflake-Badges@users.noreply.github.com>
2023-10-30 11:29:44 +0100

    Update streamlit_app.py
    Adding a second text entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_fruit_list.index))
-added_fruit_to_show = my_fruit_list.loc[add_my_fruit]
-streamlit.dataframe(added_fruit_to_show) 
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+streamlit.write('Thanks for adding',add_my_fruit)
